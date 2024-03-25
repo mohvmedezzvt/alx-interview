44b190d291654c81280aff346486c97946d6cb4d
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2024-03-26 01:25:26 +0200

    Minimum Operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Calculates the fewest number of operations needed
+to result in exactly n H characters in the file.
+"""
+
+
+def minOperations(n: int) -> int:
+    """
+    Returns the fewest number of operations needed
+    to result in exactly n H characters in the file.
+    """
+    if n <= 1:
+        return 0
+    for i in range(2, n + 1):
+        if n % i == 0:
+            return minOperations(n // i) + i
+    return n
