e13773891867f143807468086f57496edae84f63
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2024-04-08 04:55:41 +0200

    UTF-8 Validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+UTF-8 Validation
+"""
+
+from typing import List
+
+
+def validUTF8(data: List[int]) -> bool:
+    """
+    Determines if a given data set represents a valid UTF-8 encoding
+    """
+    num_bytes = 0
+
+    for num in data:
+        mask = 1 << 7
+        if not num_bytes:
+            while mask & num:
+                num_bytes += 1
+                mask = mask >> 1
+
+            if not num_bytes:
+                continue
+
+            if num_bytes == 1 or num_bytes > 4:
+                return False
+        else:
+            if num >> 6 != 0b10:
+                return False
+        num_bytes -= 1
+
+    return num_bytes == 0
