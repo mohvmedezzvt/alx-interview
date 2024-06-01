e6090c3b440e2f86dfe7e451ba7c021b0828a0cd
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2024-06-01 23:56:38 +0300

    a function to rotate a 2D matrix in-place.

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -12,26 +12,13 @@ def rotate_2d_matrix(matrix: list[list[int]]) -> None:
     Returns:
         None: The function modifies the matrix in-place.
     """
-    left, right = 0, len(matrix) - 1
+    n = len(matrix)
 
-    while left < right:
-        for i in range(right - left):
-            top, bottom = left, right
+    # Transpose the matrix
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
-            # save the topLeft
-            topLeft = matrix[top][left + i]
-
-            # move bottom left to top left
-            matrix[top][left + i] = matrix[bottom - i][left]
-
-            # move bottom right to bottom left
-            matrix[bottom - i][left] = matrix[bottom][right - i]
-
-            # move top right to bottom right
-            matrix[bottom][right - i] = matrix[top + i][right]
-
-            # move top left to top right
-            matrix[top + i][right] = topLeft
-
-        left += 1
-        right -= 1
+    # Reverse each row
+    for i in range(n):
+        matrix[i].reverse()
